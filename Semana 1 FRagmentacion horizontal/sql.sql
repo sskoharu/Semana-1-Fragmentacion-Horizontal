b8471d8c0de838b572f72b1c9b670d1819dd160c
sskoharu <134174547+sskoharu@users.noreply.github.com>
2024-06-02 21:48:28 -0500

    Add files via upload

---
+++ Semana 1 FRagmentacion horizontal/sql.sql
@@ -0,0 +1,145 @@
+-- Crear la tabla original
+CREATE TABLE alumno (
+    codigo int(5) NOT NULL,
+    cedula varchar(15) DEFAULT NULL,
+    nombres varchar(40) DEFAULT NULL,
+    apellidos varchar(40) DEFAULT NULL,
+    direccion varchar(40) DEFAULT NULL,
+    telefono1 varchar(15) DEFAULT NULL,
+    telefono2 varchar(15) DEFAULT NULL,
+    ciudad varchar(50) DEFAULT NULL,
+    provincia varchar(50) CHARACTER SET utf8 DEFAULT NULL,
+    pais varchar(50) DEFAULT NULL,
+    email varchar(50) DEFAULT NULL,
+    fechanacimiento date DEFAULT NULL,
+    redsocial varchar(15) DEFAULT NULL,
+    carrera varchar(40) DEFAULT NULL,
+    extension varchar(15) DEFAULT NULL,
+    aniograduacion int(4) DEFAULT NULL,
+    lugardetrabajo varchar(40) DEFAULT NULL,
+    direcciontrabajo varchar(40) DEFAULT NULL,
+    telefono varchar(15) DEFAULT NULL,
+    cargo varchar(40) DEFAULT NULL,
+    Empresapropia varchar(5) DEFAULT NULL,
+    trabajaensuareadeestudio varchar(5) DEFAULT NULL,
+    tiempodetrabajo int(2) DEFAULT NULL,
+    s1 text, s2 text, s3 text, s4 text, s5 text,
+    s6 text, s7 text, s8 text, s9 text, s10 text,
+    s11 text, s12 datetime DEFAULT NULL, s13 text, s14 text,
+    s15 text, s16 text, s17 text, s18 text, s19 text,
+    s20 text, s21 text, s22 text, s23 text, s24 text,
+    s25 text, s26 text, s27 text, s28 text, s29 text,
+    s30 text, s31 text, s32 text, s33 text, fechagrado date DEFAULT NULL,
+    s36 text, s37 text, s38 text, s39 text, s40 text,
+    s41 text, s42 text, s43 text, s44 text, s45 text,
+    s46 text, area1 varchar(50) DEFAULT NULL, area2 varchar(50) DEFAULT NULL,
+    s48 text, s45_1 text, s46_1 text, s47_1 text, s48_1 text,
+    s49_1 text, s50_1 text, valida varchar(2) DEFAULT NULL,
+    validac varchar(2) DEFAULT NULL, validad varchar(2) DEFAULT NULL,
+    validas varchar(2) DEFAULT NULL, validat varchar(2) DEFAULT NULL,
+    ins_even varchar(2) DEFAULT NULL, par_even varchar(2) DEFAULT NULL,
+    PRIMARY KEY (codigo),
+    KEY index_cedula (cedula)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- Crear las tablas fragmentadas
+CREATE TABLE alumno_es (
+    codigo int(5) NOT NULL,
+    cedula varchar(15) DEFAULT NULL,
+    nombres varchar(40) DEFAULT NULL,
+    apellidos varchar(40) DEFAULT NULL,
+    direccion varchar(40) DEFAULT NULL,
+    telefono1 varchar(15) DEFAULT NULL,
+    telefono2 varchar(15) DEFAULT NULL,
+    ciudad varchar(50) DEFAULT NULL,
+    provincia varchar(50) CHARACTER SET utf8 DEFAULT NULL,
+    pais varchar(50) DEFAULT NULL,
+    email varchar(50) DEFAULT NULL,
+    fechanacimiento date DEFAULT NULL,
+    redsocial varchar(15) DEFAULT NULL,
+    carrera varchar(40) DEFAULT NULL,
+    extension varchar(15) DEFAULT NULL,
+    aniograduacion int(4) DEFAULT NULL,
+    lugardetrabajo varchar(40) DEFAULT NULL,
+    direcciontrabajo varchar(40) DEFAULT NULL,
+    telefono varchar(15) DEFAULT NULL,
+    cargo varchar(40) DEFAULT NULL,
+    Empresapropia varchar(5) DEFAULT NULL,
+    trabajaensuareadeestudio varchar(5) DEFAULT NULL,
+    tiempodetrabajo int(2) DEFAULT NULL,
+    s1 text, s2 text, s3 text, s4 text, s5 text,
+    s6 text, s7 text, s8 text, s9 text, s10 text,
+    s11 text, s12 datetime DEFAULT NULL, s13 text, s14 text,
+    s15 text, s16 text, s17 text, s18 text, s19 text,
+    s20 text, s21 text, s22 text, s23 text, s24 text,
+    s25 text, s26 text, s27 text, s28 text, s29 text,
+    s30 text, s31 text, s32 text, s33 text, fechagrado date DEFAULT NULL,
+    s36 text, s37 text, s38 text, s39 text, s40 text,
+    s41 text, s42 text, s43 text, s44 text, s45 text,
+    s46 text, area1 varchar(50) DEFAULT NULL, area2 varchar(50) DEFAULT NULL,
+    s48 text, s45_1 text, s46_1 text, s47_1 text, s48_1 text,
+    s49_1 text, s50_1 text, valida varchar(2) DEFAULT NULL,
+    validac varchar(2) DEFAULT NULL, validad varchar(2) DEFAULT NULL,
+    validas varchar(2) DEFAULT NULL, validat varchar(2) DEFAULT NULL,
+    ins_even varchar(2) DEFAULT NULL, par_even varchar(2) DEFAULT NULL,
+    PRIMARY KEY (codigo),
+    KEY index_cedula (cedula)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+CREATE TABLE alumno_mx (
+    codigo int(5) NOT NULL,
+    cedula varchar(15) DEFAULT NULL,
+    nombres varchar(40) DEFAULT NULL,
+    apellidos varchar(40) DEFAULT NULL,
+    direccion varchar(40) DEFAULT NULL,
+    telefono1 varchar(15) DEFAULT NULL,
+    telefono2 varchar(15) DEFAULT NULL,
+    ciudad varchar(50) DEFAULT NULL,
+    provincia varchar(50) CHARACTER SET utf8 DEFAULT NULL,
+    pais varchar(50) DEFAULT NULL,
+    email varchar(50) DEFAULT NULL,
+    fechanacimiento date DEFAULT NULL,
+    redsocial varchar(15) DEFAULT NULL,
+    carrera varchar(40) DEFAULT NULL,
+    extension varchar(15) DEFAULT NULL,
+    aniograduacion int(4) DEFAULT NULL,
+    lugardetrabajo varchar(40) DEFAULT NULL,
+    direcciontrabajo varchar(40) DEFAULT NULL,
+    telefono varchar(15) DEFAULT NULL,
+    cargo varchar(40) DEFAULT NULL,
+    Empresapropia varchar(5) DEFAULT NULL,
+    trabajaensuareadeestudio varchar(5) DEFAULT NULL,
+    tiempodetrabajo int(2) DEFAULT NULL,
+    s1 text, s2 text, s3 text, s4 text, s5 text,
+    s6 text, s7 text, s8 text, s9 text, s10 text,
+    s11 text, s12 datetime DEFAULT NULL, s13 text, s14 text,
+    s15 text, s16 text, s17 text, s18 text, s19 text,
+    s20 text, s21 text, s22 text, s23 text, s24 text,
+    s25 text, s26 text, s27 text, s28 text, s29 text,
+    s30 text, s31 text, s32 text, s33 text, fechagrado date DEFAULT NULL,
+    s36 text, s37 text, s38 text, s39 text, s40 text,
+    s41 text, s42 text, s43 text, s44 text, s45 text,
+    s46 text, area1 varchar(50) DEFAULT NULL, area2 varchar(50) DEFAULT NULL,
+    s48 text, s45_1 text, s46_1 text, s47_1 text, s48_1 text,
+    s49_1 text, s50_1 text, valida varchar(2) DEFAULT NULL,
+    validac varchar(2) DEFAULT NULL, validad varchar(2) DEFAULT NULL,
+    validas varchar(2) DEFAULT NULL, validat varchar(2) DEFAULT NULL,
+    ins_even varchar(2) DEFAULT NULL, par_even varchar(2) DEFAULT NULL,
+    PRIMARY KEY (codigo),
+    KEY index_cedula (cedula)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- Insertar datos en las tablas fragmentadas
+INSERT INTO alumno_es
+SELECT *
+FROM alumno
+WHERE pais = 'España';
+
+INSERT INTO alumno_mx
+SELECT *
+FROM alumno
+WHERE pais = 'México';
+
+-- Verificar los datos
+SELECT * FROM alumno_es;
+SELECT * FROM alumno_mx;
